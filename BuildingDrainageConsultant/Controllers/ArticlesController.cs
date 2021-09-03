a4ffcb9840bcf0c0d81f85e681b4a85111ca4477
Alexander Atanasoff <alexander.atanasoff@gmail.com>
2021-09-03 10:05:32 +0300

    Removed Articles from Admin Area

--- BuildingDrainageConsultant/Controllers/ArticlesController.cs
+++ BuildingDrainageConsultant/Controllers/ArticlesController.cs
@@ -1,10 +1,17 @@
 ﻿namespace BuildingDrainageConsultant.Controllers
 {
     using AutoMapper;
+    using BuildingDrainageConsultant.Infrastructure;
     using BuildingDrainageConsultant.Models.Articles;
     using BuildingDrainageConsultant.Services.Articles;
+    using BuildingDrainageConsultant.Services.Articles.Models;
+    using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
+    using System.Collections.Generic;
 
+    using static Areas.Admin.AdminConstants;
+
+    [Authorize(Roles = AdministratorRoleName)]
     public class ArticlesController : Controller
     {
         private readonly IArticleService articles;
@@ -18,6 +25,77 @@
             this.mapper = mapper;
         }
 
+        public IActionResult Add() => View();
+
+        [HttpPost]
+        public IActionResult Add(ArticleFormModel article)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(article);
+            }
+
+            this.articles.Create(
+                article.Title,
+                article.Content,
+                article.ImageUrl);
+
+            return RedirectToAction(nameof(All));
+        }
+
+        public IActionResult All(IEnumerable<ArticleServiceModel> query)
+        {
+            query = this.articles.All();
+
+            return View(query);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var article = this.articles.Details(id);
+
+            var articleForm = this.mapper.Map<ArticleFormModel>(article);
+
+            return View(articleForm);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(int id, ArticleFormModel article)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(article);
+            }
+
+            if (!User.IsAdmin())
+            {
+                return BadRequest();
+            }
+
+            this.articles.Edit(
+                id,
+                article.Title,
+                article.Content,
+                article.ImageUrl);
+
+            return RedirectToAction(nameof(All));
+        }
+
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var article = this.articles.Delete(id);
+
+            if (article == false)
+            {
+                return NotFound();
+            }
+
+            return RedirectToAction(nameof(All));
+        }
+
+        [AllowAnonymous]
         public IActionResult Details(int id)
         {
             var drain = this.articles.Details(id);
